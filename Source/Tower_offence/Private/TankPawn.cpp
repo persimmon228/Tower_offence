8e58f09d8b151f59d652ad95d50734123d7bfd3f
Danil <d20004k@gmail.com>
2024-04-23 16:28:56 +0200

    Task 4 last com

--- Source/Tower_offence/Private/TankPawn.cpp
+++ Source/Tower_offence/Private/TankPawn.cpp
@@ -11,7 +11,7 @@ ATankPawn::ATankPawn()
 	//Create Spring Arm and Setup Attachment
 	SpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraSpringArm"));
 	SpringArm->AttachToComponent(RootComponent, FAttachmentTransformRules::KeepRelativeTransform);
-	SpringArm->TargetArmLength = 250.0f;
+	SpringArm->TargetArmLength = 350.0f;
 	SpringArm->bEnableCameraLag = true;
 	SpringArm->CameraLagSpeed = 1.5f;
 
@@ -23,7 +23,7 @@ ATankPawn::ATankPawn()
 
 void ATankPawn::Move(float DistansToMove)
 {
-	FVector ConvertedVector(0.0f, (DistansToMove*10), 0.0f);
+	FVector ConvertedVector((DistansToMove * 10), 0.0f, 0.0f);
 	//Setting new Location
 	AddActorLocalOffset(ConvertedVector);
 }
@@ -33,6 +33,7 @@ void ATankPawn::Turn(float Rotation)
 	FRotator ConvertedVector(0.0f, Rotation, 0.0f);
 	//Setting new Rotation
 	this->SetActorRotation((this->GetActorRotation() + ConvertedVector));
+	MyTurretMesh->SetWorldRotation((MyTurretMesh->GetComponentRotation() - ConvertedVector));
 }
 
 void ATankPawn::TurrentRotation(float YawValue, float PitchValue)
